98817f8a04ba383ed2af1c282fbd8c41cde10ddc
Ase-Medina <arivas@chih.conalep.edu.mx>
2025-05-13 07:46:29 -0600

    mi primer programa

---
+++ script.js
@@ -0,0 +1,39 @@
+function sumar(){
+    var num1;
+    num1= document.calc.txtnumero1.value;
+    var num2;
+    num2= document.calc.txtnumero2.value;
+    var opc;
+    opc= eval(num1) + eval(num2);
+    document.calc.resultado.value=opc;
+    }
+
+function restar(){
+        var num1;
+        num1= document.calc.txtnumero1.value;
+        var num2;
+        num2= document.calc.txtnumero2.value;
+        var opc;
+        opc= eval(num1) - eval(num2);
+        document.calc.resultado.value=opc;
+        }
+
+ function dividir(){
+            var num1;
+            num1= document.calc.txtnumero1.value;
+            var num2;
+            num2= document.calc.txtnumero2.value;
+            var opc;
+            opc= eval(num1) / eval(num2);
+            document.calc.resultado.value=opc;
+            }
+
+ function multiplicar(){
+                var num1;
+                num1= document.calc.txtnumero1.value;
+                var num2;
+                num2= document.calc.txtnumero2.value;
+                var opc;
+                opc= eval(num1) * eval(num2);
+                document.calc.resultado.value=opc;
+                }
